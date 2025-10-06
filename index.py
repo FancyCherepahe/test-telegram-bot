e48971a21e4264a0485ed49608fd49949c64f816
FancyCherepahe <elektrodanulo@gmail.com>
2025-10-06 21:31:46 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,5 +1,9 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+import os
+
+PORT = int(os.environ.get("PORT", 8443))
+
 
 BOT_TOKEN = "8355649306:AAEkSbV0ExKwU9Kbgqr0j8-lfY12q2IeaUk"  # Replace with your actual token
 
@@ -10,3 +14,4 @@ app = ApplicationBuilder().token(BOT_TOKEN).build()
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, hello))
 
 app.run_polling()
+app.run(port=PORT)
